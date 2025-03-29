2acb74298d06ad460546cb9f49967753d35b8a11
6476173854 <144263575+6476173854@users.noreply.github.com>
2025-03-29 15:43:27 -0500

    done

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,13 +1,12 @@
 module.exports = {
-    themeConfig: {
-      nav: [
-        { text: 'Home', link: '/' },
-        { text: 'About', link: '/about/' },
-        { text: 'Projects', link: '/projects/' },
-        { text: 'Contact', link: '/contact/' },
-        { text: 'Blog', link: '/blog/' },
-        { text: 'Hobbies', link: '/hobbies/' }
-      ]
-    }
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'About', link: '/about/' },
+      { text: 'Projects', link: '/projects/' },
+      { text: 'Contact', link: '/contact/' },
+      { text: 'Blog', link: '/blog/' },
+      { text: 'Hobbies', link: '/hobbies/' }
+    ]
   }
-  
+}
--- docs/contact.md
+++ docs/contact.md
@@ -3,7 +3,7 @@
 Feel free to reach out to me if you have any questions, want to collaborate on a project, or just want to chat about tech.
 
 ### Email:
-- ankushbansal@example.com
+- ankushbansal3762gmail.com
 
 ### LinkedIn:
 - [LinkedIn Profile](https://www.linkedin.com/in/ankushbansal)
